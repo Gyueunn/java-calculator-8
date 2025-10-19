3b6584280f53c9f9e46a548b9b868f632c06d1f4
Gyueun <103631319+Gyueunn@users.noreply.github.com>
2025-10-19 22:27:16 +0900

    style: 변수명 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,23 +11,23 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         input_text  = Console.readLine();
 
-        if(input_text == null && input_text.length() == 0) {// 입력 값 공백 예외 처리
+        if(input_text == null || input_text.isEmpty()) {// 입력 값 공백 예외 처리
             throw new IllegalArgumentException("입력 값이 없습니다.");
         }
 
         array_word = input_text.split(",|:");
-        int[] array_nums = new int[array_word.length];
+        int[] array_num = new int[array_word.length];
 
         for(int i = 0; i < array_word.length; i++) {// 공백 포함 예외 처리
             if (array_word[i].contains(" ")) {
                 throw new IllegalArgumentException("공백이 입력될 수 없습니다.");
             }
-            array_nums[i] = Integer.parseInt(array_word[i]);
+            array_num[i] = Integer.parseInt(array_word[i]);
 
-            if (array_nums[i] < 0) {// 음수 예외 처리
+            if (array_num[i] < 0) {// 음수 예외 처리
                 throw new IllegalArgumentException("음수가 입력될 수 없습니다.");
             }
-            total_sum += array_nums[i];
+            total_sum += array_num[i];
         }
         System.out.println("결과 : " + total_sum);
     }
